becaeb7ed9706888b55051d409a4f1461ed47cc7
Magnus Andersson <gusbannbma@student.gu.se>
2018-04-14 14:18:57 +0200

    Add Worm and player

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ const physics = new Physics();
 const map = new Map(800,600);
 
 function main() {
-    map.simpleMap();
-    var b = new Bazooka();
-    console.log(b.use(new Vector(100, 600), new Vector(0,0)));
+    canvas.show(map);
+    //var b = new Bazooka();
+    var p = new Player();
+    p.worms[0].move(1);
+    console.log(p.worms[0].direction);
+    //console.log(b.use(new Vector(100, 600), new Vector(0,0)));
 }
 
 main();
