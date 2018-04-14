873ddca327cddfc362757213326bae94ca597fe6
Magnus Andersson <gusbannbma@student.gu.se>
2018-04-14 14:39:45 +0200

    Fix bugs in Player and Worm

--- src/player/Player.js
+++ src/player/Player.js
@@ -1,11 +1,18 @@
 function Player(){
     this.worms = [
-        new Worm(1,1, new Bazooka()),
-        new Worm(2,2, new Bazooka()),
-        new Worm(3,3, new Bazooka()),
-        new Worm(4,4, new Bazooka()),
-        new Worm(5,5, new Bazooka())
+        new Worm(350, 350, new Bazooka()),
+        new Worm(map.grid.sizex/2 - 3,map.grid.sizey/2 + 2, new Bazooka()),
+        new Worm(map.grid.sizex/2 - 1,map.grid.sizey/2 + 3, new Bazooka()),
+        new Worm(map.grid.sizex/2 + 1,map.grid.sizey/2 + 4, new Bazooka()),
+        new Worm(map.grid.sizex/2 + 3,map.grid.sizey/2 + 5, new Bazooka())
     ];
 }
 
 Player.prototype.constructor = Player;
+
+Player.prototype.update = function(){
+    //Check if any worms are dead and do something with them
+    this.worms.forEach(worm => {
+        console.log(worm.status);
+    });
+}
