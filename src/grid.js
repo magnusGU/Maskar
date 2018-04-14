03b5ef11a7ca140d35ad23f54de5ce0e8443b92d
klas <klas@ludvigsson.se>
2018-04-14 10:35:37 +0200

    dsa

---
+++ src/grid.js
@@ -0,0 +1,10 @@
+class Map {
+    constructor(sizex,sizey){
+        this.grid = new Array(sizex);
+        for(var i = 0; i < sizex; i++){
+            grid[i] = new Array(sizey);
+        }
+    }
+}
+
+Console.log("hej");
