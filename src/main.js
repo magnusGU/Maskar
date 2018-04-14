c5d7461eac4b0f45aa570d59204ff615d71ed2e4
Magnus Andersson <gusbannbma@student.gu.se>
2018-04-14 14:20:22 +0200

    Change a comment for better flow and readability

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ const map = new Map(800,600);
 function main() {
     canvas.show(map);
     //var b = new Bazooka();
+    //console.log(b.use(new Vector(100, 600), new Vector(0,0)));
     var p = new Player();
     p.worms[0].move(1);
     console.log(p.worms[0].direction);
-    //console.log(b.use(new Vector(100, 600), new Vector(0,0)));
 }
 
 main();
